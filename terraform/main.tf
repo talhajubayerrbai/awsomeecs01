3ae41132abfa3a0162029433d17d2e91ab5703a7
talhajubayerrbai <talha.jubayer@royalbengal.ai>
2026-07-06 22:07:39 +0600

    Auto-fix attempt 1: .github/workflows/deploy.yml, terraform/main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -169,11 +169,6 @@ resource "aws_route_table" "private" {
 }
 
 resource "aws_route_table_association" "private" {
-  subnet_id      = aws_route_table.private.id
-  route_table_id = aws_route_table.private.id
-}
-
-resource "aws_route_table_association" "private_subnet" {
   subnet_id      = aws_subnet.private.id
   route_table_id = aws_route_table.private.id
 }
